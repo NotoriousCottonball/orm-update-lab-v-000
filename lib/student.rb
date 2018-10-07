48169f0b06c1cf14096a4ea7f85a2b72e00630ea
George  Pianka <gpianka1@gmail.com>
2018-10-07 09:28:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,6 +43,9 @@ class Student
     self.new(row[0], row[1], row[2])
   end
   
-
+  def self.find_by_name(name)
+    student = DB[:conn].execute("SELECT * FROM students WHERE name = ?", name).flatten
+    self.new_from_db(student)
+  end
 
 end
