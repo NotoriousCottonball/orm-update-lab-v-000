91ab8275cf9f1417abd0ea63145b1f9a7cd0e653
George  Pianka <gpianka1@gmail.com>
2018-10-07 09:24:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,11 +33,13 @@ class Student
     end
   end
   
-   def self.create(name:, grade:)
+  def self.create(name:, grade:)
     student = self.new(name, grade)
     student.save
     student
   end
+  
+  
   
 
 
